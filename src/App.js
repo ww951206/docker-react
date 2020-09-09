d931988dbebc6c09e994e4b20e149f9cc082a5aa
ww951206 <mikcing@Gmail.com>
2020-09-09 13:35:29 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React(with some changed feature)
         </a>
       </header>
     </div>
